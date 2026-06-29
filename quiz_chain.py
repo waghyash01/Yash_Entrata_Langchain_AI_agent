db80e3c41e20505472d3fd546b845304ff84a869
waghyash01 <123620840+waghyash01@users.noreply.github.com>
2026-06-29 15:01:45 +0530

    Update quiz_chain.py

--- quiz_chain.py
+++ quiz_chain.py
@@ -7,7 +7,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from models import Quiz
-
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
